9af36b14d7f409e468b6f1c84c7768134efb94fd
Pedro Freitas <pedrinfreitas@gmail.com>
2022-04-02 20:03:00 -0300

    feat: query graphql

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,64 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script context="module" lang="ts">
+  import { request, gql } from "graphql-request";
+
+  export interface ICountries {
+    countries: ICountry[];
+  }
+  interface ICountry {
+    name: string;
+    emoji: string;
+  }
+
+  const query = gql`
+    {
+      countries {
+        name
+        emoji
+      }
+    }
+  `;
+
+  export const load = async () => {
+    const data: ICountries = await request(
+      "https://countries.trevorblades.com/",
+      query
+    );
+
+    return {
+      props: {
+        data,
+      },
+    };
+  };
+
+  /**
+   * USANDO O FETCH DE FORMA NATIVA
+   */
+  //   export const load = async ({ fetch }) => {
+  //     const res = await fetch("https://countries.trevorblades.com/", {
+  //       method: "POST",
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //       },
+  //       body: JSON.stringify({ query }),
+  //     });
+
+  //     const { data } = await res.json();
+
+  //     return {
+  //       props: {
+  //         data,
+  //       },
+  //     };
+  //   };
+</script>
+
+<script lang="ts">
+  export let data: ICountries;
+</script>
+
+<h1>Countries</h1>
+
+{#each data.countries as country}
+  <p>{country.name} - {country.emoji}</p>
+{/each}
